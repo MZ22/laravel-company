7ddac074a55cca91c11f2337db4f5021c9b8d350
MZ22 <maryam.ramzi.m@gmail.com>
2019-03-19 11:44:00 +0100

    data changes

--- public/index.php
+++ public/index.php
@@ -23,11 +23,12 @@ define('LARAVEL_START', microtime(true));
 
 require __DIR__.'/../vendor/autoload.php';
 
+
 /*
 |--------------------------------------------------------------------------
 | Turn On The Lights
 |--------------------------------------------------------------------------
-|
+| 
 | We need to illuminate PHP development, so let us turn on the lights.
 | This bootstraps the framework and gets it ready for use, then it
 | will load up this application so that we can run it and send
